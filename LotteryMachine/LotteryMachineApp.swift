6091078c035c2e61e8131438f3b15c198aa9b7c7
Noah lin <noah@c-link.com.tw>
2025-11-21 14:06:51 +0800

    add SettingsView.

--- LotteryMachine/LotteryMachineApp.swift
+++ LotteryMachine/LotteryMachineApp.swift
@@ -13,5 +13,10 @@ struct LotteryMachineApp: App {
         WindowGroup {
             ContentView()
         }
+        #if os(macOS)
+            Settings {
+                SettingsView()
+            }
+        #endif
     }
 }
---
+++ LotteryMachine/Views/SettingsView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingView.swift
+//  LotteryMachine
+//
+//  Created by Noah on 2025/11/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("SettingView")
+    }
+}
